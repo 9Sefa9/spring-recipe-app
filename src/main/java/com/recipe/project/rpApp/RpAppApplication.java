b4ebbbf62951234ca87419a47d7c3f090ddf134d
GROUPINFRA\sefa.goevercin <sefa.goevercin@cgi.com>
2022-05-10 11:30:29 +0200

    ..

---
+++ src/main/java/com/recipe/project/rpApp/RpAppApplication.java
@@ -0,0 +1,12 @@
+package com.recipe.project.rpApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/recipe/project/rpApp/RpAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.project.rpApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
